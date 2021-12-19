24aede51feed2e1b1c2dd3bee8f9a263e4167ef2
Steven Garcia <sjosegarcia23@gmail.com>
2021-12-19 16:34:31 -0500

    Gas

---
+++ contracts/Gas.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.8.10;
+
+contract Gas {
+    uint public i = 0;
+
+    //gasPayment = gas spent * gas price
+    //gas spent = total amount of gas used in txn
+    //gas price is how much ether you are willing to pay PER gas
+
+    function forever() public { //loop until all the gas is spent and txn fails
+        while (true) i += 1;
+    }
+}
