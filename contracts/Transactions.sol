db2523bbcf78cee246c488478a94f64672a23ddf
Steven Garcia <sjosegarcia23@gmail.com>
2022-02-28 14:03:46 -0500

    added some important comments about blocktime

--- contracts/Transactions.sol
+++ contracts/Transactions.sol
@@ -15,13 +15,14 @@ contract Transactions {
         string keyword;
     }
 
-    uint256 transactionCount;
-    TransferStruct[] transactions;
+    uint256 private transactionCount;
+    TransferStruct[] private transactions;
 
     function addToBlockchain(address payable receiver, uint amount, string memory message, string memory keyword) public {
         transactionCount += 1;
-        transactions.push(TransferStruct(msg.sender, receiver, amount, message, block.timestamp, keyword));
-        emit Transfer(msg.sender, receiver, amount, message, block.timestamp, keyword);
+        uint time = block.timestamp;
+        transactions.push(TransferStruct(msg.sender, receiver, amount, message, time, keyword));
+        emit Transfer(msg.sender, receiver, amount, message, time, keyword);
     }
 
     function getAllTransactiond() public view returns (TransferStruct[] memory) {
@@ -32,3 +33,22 @@ contract Transactions {
         return transactionCount;
     }
 }
+
+/**
+
+    block.timestamp cannot be used safely or reliably
+
+    why?
+
+    If your block is stamped with a time too far in the future, newer blocks wont be added to it by the miners
+
+
+    Your block time cannot be stamped with an earlier time than the previous (ofc)
+
+    Difficulty is kept lowest by not stamping blokcs as earlier than they actually occur
+
+    Powerful miners can manipulate timestamps over short period especially if they have something to gain from it
+
+    Lower hashrate miners will produce close to accurate blocktime
+
+ */
