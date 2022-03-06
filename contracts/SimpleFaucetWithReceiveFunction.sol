06c7d15537dbba63202a4459138b87e4ac34467e
Steven Garcia <sjosegarcia23@gmail.com>
2022-03-05 19:05:01 -0500

    added event

--- contracts/SimpleFaucetWithReceiveFunction.sol
+++ contracts/SimpleFaucetWithReceiveFunction.sol
@@ -2,10 +2,15 @@ pragma solidity ^0.8.10;
 
 contract SimpleFaucetWithReceiveFunction {
 
+    event Received(address, uint);
+    
     function payout(uint amount) public {
         require(amount <= 10000000000000000000, "Incorrect input for amount");
         payable(msg.sender).transfer(amount);
 
     }
-    receive() external payable {}
+
+    receive() external payable {
+        emit Received(msg.sender, msg.value);
+    }
 }
