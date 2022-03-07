c236792151f2a711697e080615b0a63826969163
Steven Garcia <sjosegarcia23@gmail.com>
2022-03-07 10:14:37 -0500

    Favorite Number contract, custom

---
+++ contracts/FavoriteNumber.sol
@@ -0,0 +1,31 @@
+pragma solidity ^0.8.10;
+
+contract FavoriteNumber {
+
+    struct Person {
+        string name;
+        uint256 favoriteNumber;
+    }
+
+    mapping(address => Person) public addressToPerson;
+
+    function store(string memory name, uint256 favoriteNumber) public {
+        require(bytes(name).length > 0, "Please provide a nanme");
+        require(favoriteNumber > 0, "Please provide a favorite number");
+        addressToPerson[msg.sender] = Person(name, favoriteNumber);
+    }
+
+    function getPersonName() public view returns (string memory name) {
+        return addressToPerson[msg.sender].name;
+    }
+
+    function getPersonFavoriteNumber() public view returns (uint256 favoriteNumber) {
+        return addressToPerson[msg.sender].favoriteNumber;
+    }
+
+    function changeFavoriteNumber(uint256 favoriteNumber) public {
+        require(favoriteNumber > 0, "Please provide a favorite number");
+        addressToPerson[msg.sender].favoriteNumber = favoriteNumber;
+    }
+
+}
