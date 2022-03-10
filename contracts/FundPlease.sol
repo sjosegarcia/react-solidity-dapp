b3bc8e19339aebb4a53d75b47ce9697c224f4318
Steven Garcia <sjosegarcia23@gmail.com>
2022-03-09 22:32:59 -0500

    Funding Contract keeps track of deposits

--- contracts/FundPlease.sol
+++ contracts/FundPlease.sol
@@ -2,7 +2,8 @@ pragma solidity ^0.8.10;
 
 contract FundPlease {
 
-    mapping(address => uint256) public fundedAmount;
+    mapping(address => uint256) public addressToFundAmount;
+    address[] private funders;
     address private owner;
 
     constructor() {
@@ -10,6 +11,22 @@ contract FundPlease {
     }
 
     function fundContract() public payable {
-        fundedAmount[msg.sender] += msg.value;
+        addressToFundAmount[msg.sender] += msg.value;
+        funders.push(msg.sender);
     }
+
+    modifier onlyOwner() {
+        require(owner == msg.sender, "You are not contract owner.");
+        _;
+    }
+    function withdraw() public onlyOwner payable {
+        for (uint256 i = 0; i < funders.length; i++) {
+            address funder = funders[i];
+            addressToFundAmount[funder] = 0;
+        }
+        funders = new address[](0);
+        require(funders.length == 0, "Data did not clear.");
+        payable(msg.sender).transfer(address(this).balance);
+    }
+
 }
